0b1cd568f90c17e4fc62ce89bcb2036c2098ad84
whjeonlocal <whjeonlocal@gmail.com>
2024-02-10 15:40:31 +0900

    [전우형] 기본 웹페이지 추가

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+//연습)index.html 파일 기본 보여주기위해 추가
+//주의 순서가 변경되면 안됨!
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.Run();
