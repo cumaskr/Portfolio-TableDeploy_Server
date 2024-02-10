637391d7b939e309a74f711b5b8dcbfd12298483
whjeonlocal <whjeonlocal@gmail.com>
2024-02-10 16:37:42 +0900

    [전우형] ApiController 추가 - 호출테스트 완료

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIServer
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> GetJson(string jsonName)
+        {
+            return Ok($"GetJson/{jsonName}");
+        }
+
+        [HttpGet]
+        public ActionResult<string> Test()
+        {
+            return Ok("Test");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//컨트롤러 기능 추가
+builder.Services.AddControllers();
+
+//서버 셋팅
 var app = builder.Build();
 
-//연습)index.html 파일 기본 보여주기위해 추가
-//주의 순서가 변경되면 안됨!
+//연습)index.html 파일 기본 보여주기위해 추가, 주의 순서가 변경되면 안됨!
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
+//Controller 매핑
+app.MapControllers();
+
+//서버 시작
 app.Run();
