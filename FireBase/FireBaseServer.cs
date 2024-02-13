7be36947a74134077222e09f72cae235a2fd20ee
whjeonlocal <whjeonlocal@gmail.com>
2024-02-13 12:28:51 +0900

    [전우형] 파이어베이스 리스너 등록 추가

---
+++ FireBase/FireBaseServer.cs
@@ -0,0 +1,54 @@
+﻿using Google.Cloud.Firestore;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections;
+
+namespace APIServer
+{
+    /// <summary>
+    /// FireBase(저장소) 클래스[whjeon 24.02.13]
+    /// 클라이언트 프로젝트와 설정값은 동일하여, 후에 공용화 해보자
+    /// </summary>
+    public class FireBaseServer
+    {
+        //저장소 접근하기위한 Json파일명(해당 파일은 깃에 올라가지 않음, 특정 컴퓨터 내부에서 관리)
+        private string AdminSdkJson = "tablebuild-e6f20-firebase-adminsdk-xx0ap-d795853497.json";
+        private string ProjectId = "tablebuild-e6f20";
+        private string Server = "Local";
+        private string DocumentName = "Table";
+
+        //저장소 객체
+        public FirestoreDb? Db { get; private set; }
+        //저장소 리스너
+        private FirestoreChangeListener? Listner { get; set; }
+        //로그
+        private ILogger Logger { get; set; }
+
+        public FireBaseServer(ILogger logger)
+        {
+            //로그 셋팅
+            Logger = logger;            
+            //파이어베이스 리스너 등록
+            string path = $"./{AdminSdkJson}";
+            if (File.Exists(path)) 
+            {
+                logger.LogInformation("[FireBase] Start Setting");
+
+                Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+                Db = FirestoreDb.Create(ProjectId);
+                Listner = Db.Collection(Server).Document(DocumentName).Listen(snapshot =>
+                {
+                    var dic = snapshot.ToDictionary();
+                    var str = JsonConvert.SerializeObject(dic);
+                    logger.LogInformation($"[FireBase] Call Back - {str}");
+                });
+
+                logger.LogInformation("[FireBase] Register Listner Done");
+            }
+            else
+            {
+                logger.LogInformation("[FireBase] Fail Setting");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
+using APIServer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //컨트롤러 기능 추가
 builder.Services.AddControllers();
 
-//서버 셋팅
+//파이어베이스 서버 추가(리스너 등록) - 로그추가
+var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddFilter("System", LogLevel.Trace).AddConsole();
+});
+var fs = new FireBaseServer(loggerFactory.CreateLogger<FireBaseServer>());
+builder.Services.AddSingleton(fs);
+
+//서버 빌드(셋팅)
 var app = builder.Build();
 
 //연습)index.html 파일 기본 보여주기위해 추가, 주의 순서가 변경되면 안됨!
